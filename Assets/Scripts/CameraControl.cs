1bbfe202b04e851b66ee75b8c7af88c872e82962
craus90 <craus902@gmail.com>
2022-04-10 14:36:20 +0400

    camera drag and drop

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Vector2 dragPoint;
+
+    public void Update() {
+        if (Input.GetMouseButtonDown(1)) {
+            dragPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if (Input.GetMouseButton(1)) {
+            Vector2 worldMousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Camera.main.transform.position = (Camera.main.transform.position.xy() - (worldMousePoint - dragPoint)).withZ(Camera.main.transform.position.z);
+        }
+    }
+}
