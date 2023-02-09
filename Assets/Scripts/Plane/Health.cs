0a5988fa8f122b1efd1f98a34bc2bc489392be4a
craus90 <craus902@gmail.com>
2023-02-09 17:38:26 +0400

    monster health and hearts

---
+++ Assets/Scripts/Plane/Health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(Figure))]
+public class Health : MonoBehaviour
+{
+    public List<Transform> hearts;
+
+    [SerializeField] private int current = 3;
+    [SerializeField] private int max = 3;
+
+    public int Current
+    {
+        get => current;
+        set
+        {
+            current = value;
+            UpdateHearts();
+        }
+    }
+
+    public void Awake() {
+        UpdateHearts();
+    }
+
+    private void UpdateHearts() {
+        for (int i = 0; i < hearts.Count; i++) {
+            hearts[i].gameObject.SetActive(current >= i + 1);
+            hearts[i].localPosition = new Vector3(i - current * 0.5f + 0.5f, 0, 0);
+        }
+    }
+}
