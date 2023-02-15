42481423ea46cca850c8ab63ae7503e6ae3c6167
craus90 <craus902@gmail.com>
2023-02-16 01:43:35 +0400

    teleport trap invisible until triggered close to player

--- Assets/Scripts/Plane/Terrain/TeleportTrap.cs
+++ Assets/Scripts/Plane/Terrain/TeleportTrap.cs
@@ -8,14 +8,25 @@ public class TeleportTrap : MonoBehaviour
 {
     public int teleportRadius = 6;
     public int invulnerabilityTurns = 1;
+    public int triggerVisibilityRadius = 4;
+
+    public GameObject sprite;
 
     public void Awake() {
         GetComponent<Figure>().collide = async (from, figure) => {
             var victim = figure.GetComponent<Unit>();
             if (victim != null) {
+                if ((Player.instance.figure.location.position - GetComponent<Figure>().location.position).MaxDelta() <= triggerVisibilityRadius) {
+                    Show();
+                }
+
                 await victim.GetComponent<Invulnerability>().Gain(invulnerabilityTurns);
                 await Helpers.TeleportAway(victim.figure, teleportRadius);
             }
         };
+    }
+
+    public void Show() {
+        sprite.SetActive(true);
     }
 }
