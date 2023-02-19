7b49bf8f978707c84e13babe12a69ba47eda2835
craus90 <craus902@gmail.com>
2023-02-19 05:21:21 +0400

    rapier

---
+++ Assets/Scripts/Items/Weapon/Rapier.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[RequireComponent(typeof(Item))]
+public class Rapier : Weapon
+{
+    public override Task<bool> BeforeWalk(Vector2Int delta) => TryAttack(delta);
+
+    public override IEnumerable<Vector2Int> AttackVectors() {
+        yield return new Vector2Int(2, 0);
+    }
+
+    public override async Task<bool> TryAttack(Vector2Int delta) {
+        var result = await base.TryAttack(delta);
+        if (result) {
+            if (Owner.alive) {
+                if (!await Owner.figure.TryWalk(delta)) {
+                    await Owner.figure.FakeMove(delta);
+                }
+            }
+        }
+        return result;
+    }
+}
