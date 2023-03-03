dcbc27f989e0ecdd7c4fc548ae736b46676a881e
craus90 <craus902@gmail.com>
2023-03-03 08:00:51 +0400

    dual daggers

---
+++ Assets/Scripts/Items/Weapon/DualDaggers.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[RequireComponent(typeof(Item))]
+public class DualDaggers : Weapon
+{
+    public override async Task<bool> TryAttack(Vector2Int delta) {
+        bool result = false;
+        var leftTarget = Owner.figure.location.Shift(delta.Relative(1, 1)).GetFigure<Unit>(u => u.Vulnerable);
+        var rightTarget = Owner.figure.location.Shift(delta.Relative(1, -1)).GetFigure<Unit>(u => u.Vulnerable);
+        return (await Task.WhenAll(Attack(leftTarget), Attack(rightTarget))).Any(b => b);
+    }
+
+    public override Task<bool> BeforeWalk(Vector2Int delta) => TryAttack(delta);
+}
