1efad33a09863bb54a3a15c82c4d5336ceb394c4
craus90 <craus902@gmail.com>
2023-03-05 04:09:02 +0400

    gloves of recoil

---
+++ Assets/Scripts/Items/Gloves/GlovesOfRecoil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Item))]
+public class GlovesOfRecoil : MonoBehaviour, IAttackModifier
+{
+    public int Priority => 0;
+
+    public async Task ModifyAttack(Attack attack) {
+        await attack.from.TryWalk((attack.from.location.position - attack.to.location.position).StepAtDirectionDiagonal());
+    }
+}
