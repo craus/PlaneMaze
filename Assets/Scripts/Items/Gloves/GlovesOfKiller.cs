b88005b51ff52fef29de8679475eb0d4eb2474d4
craus90 <craus902@gmail.com>
2023-03-05 04:44:33 +0400

    gloves of killer

---
+++ Assets/Scripts/Items/Gloves/GlovesOfKiller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Item))]
+public class GlovesOfKiller : MonoBehaviour, IAttackModifier
+{
+    public int Priority => 0;
+
+    public async Task ModifyAttack(Attack attack) {
+        attack.afterAttack.Add(
+            async () => {
+                if (!attack.to.GetComponent<Unit>().alive) {
+                    await attack.from.GetComponent<Invulnerability>().Gain(1);
+                }
+            }
+        );
+    }
+}
