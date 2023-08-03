76ff6b58df6f453f904e316e748ba7fad3a8019e
craus90 <craus902@gmail.com>
2023-08-04 00:43:57 +0400

    vampire description; sword not disarms enemy

--- Assets/Scripts/Items/Weapon/Dagger.cs
+++ Assets/Scripts/Items/Weapon/Dagger.cs
@@ -8,4 +8,11 @@ using UnityEngine;
 public class Dagger : Weapon
 {
     public override Task<bool> AfterFailedWalk(Vector2Int delta) => TryAttack(delta);
+
+    public override async Task AfterAttack(Attack attack) {
+        if (this != null && attack.to != null) {
+            await attack.to.GetComponent<Disarm>().Gain(1);
+        }
+        await base.AfterAttack(attack);
+    }
 }
