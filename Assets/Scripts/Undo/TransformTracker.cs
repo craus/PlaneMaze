26e5aaf26261fdcb90f4e021cd81fdf9e2412382
craus90 <craus902@gmail.com>
2023-08-05 14:46:21 +0400

    item transform tracker; game time tracoer

---
+++ Assets/Scripts/Undo/TransformTracker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TransformTracker : MonoBehaviour
+{
+    public void Awake() {
+        new ValueTracker<Vector3>(
+            () => transform.localPosition,
+            v => transform.localPosition = v
+        );
+        new ValueTracker<Quaternion>(
+            () => transform.localRotation,
+            v => transform.localRotation = v
+        );
+    }
+}
