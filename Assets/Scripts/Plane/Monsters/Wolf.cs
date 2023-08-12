4b7c2fa9db7a99b986bebf8d56ae5343d2405437
craus90 <craus902@gmail.com>
2023-08-12 04:52:21 +0400

    wolf

---
+++ Assets/Scripts/Plane/Monsters/Wolf.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Wolf : Monster
+{
+    protected override async Task MakeMove() {
+
+        Vector2Int playerDelta = Player.instance.figure.Location.position - figure.Location.position;
+
+        if (playerDelta.SumDelta() == 1) {
+            if (await TryAttack(playerDelta)) {
+                return;
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+            var delta = Moves.Rnd();
+            GetComponent<SpriteDirection>().SetDirection(delta);
+            if (!await figure.TryWalk(delta)) {
+                await figure.FakeMove(delta);
+            }
+        }
+    }
+}
