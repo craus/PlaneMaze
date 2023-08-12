d60fb909a6801d3abc330c1b42ae8035888b5ca7
craus90 <craus902@gmail.com>
2023-08-12 04:05:29 +0400

    ent

---
+++ Assets/Scripts/Plane/Monsters/Ent.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Ent : Monster
+{
+    public override int Money => 0;
+    public override bool HasSoul => false;
+    public override bool Movable => !treeForm;
+
+    public override bool Threatening => !treeForm;
+
+    public int cooldown;
+    public int currentCooldown;
+
+    public bool treeForm = true;
+    public SpriteRenderer treeSprite;
+    public GameObject entModel;
+    public GameObject activeSprite;
+    public GameObject inactiveSprite; 
+
+    public override void Awake() {
+        base.Awake();
+        currentCooldown = cooldown;
+
+        ChangeForm(tree: true);
+        new ValueTracker<bool>(() => treeForm, ChangeForm);
+
+        new ValueTracker<int>(() => currentCooldown, v => {
+            currentCooldown = v;
+            UpdateSprite();
+        });
+    }
+
+    private void UpdateSprite() {
+        activeSprite.SetActive(currentCooldown <= 1);
+        inactiveSprite.SetActive(currentCooldown > 1);
+        //actives
+    }
+
+    public override async Task<bool> TryAttack(Vector2Int delta) {
+        if (treeForm) {
+            return false;
+        }
+        return await base.TryAttack(delta);
+    }
+
+    public override async Task Hit(Attack attack) {
+        await base.Hit(attack);
+        if (alive) {
+            attack.afterAttack.Add(async () => ChangeForm(tree: false));
+        }
+    }
+
+    private void ChangeForm(bool tree) {
+        treeForm = tree;
+        treeSprite.enabled = treeForm;
+        entModel.SetActive(!tree);
+    }
+
+    protected override async Task MakeMove() {
+        if (treeForm) {
+            return;
+        }
+
+        --currentCooldown;
+        UpdateSprite();
+        if (currentCooldown > 0) {
+            return;
+        }
+
+        var playerDelta = Player.instance.figure.Location.position - figure.Location.position;
+        playerDelta = Helpers.StepAtDirection(playerDelta);
+        GetComponent<SpriteDirection>().SetDirection(playerDelta);
+        if (!await SmartWalk(playerDelta)) {
+            if (!await TryAttack(playerDelta)) {
+                await SmartFakeMove(playerDelta);
+            }
+        }
+        currentCooldown = cooldown;
+        UpdateSprite();
+    }
+}
---
+++ Assets/Scripts/Plane/SpriteDirection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[RequireComponent(typeof(Figure))]
+public class SpriteDirection : MonoBehaviour
+{
+    [SerializeField] private List<SpriteRenderer> leftSprites;
+    [SerializeField] private List<SpriteRenderer> rightSprites;
+
+    public void SetDirection(Vector2Int direction) {
+        leftSprites.ForEach(sr => sr.flipX = direction.x > 0);
+        rightSprites.ForEach(sr => sr.flipX = direction.x < 0);
+    }
+}
