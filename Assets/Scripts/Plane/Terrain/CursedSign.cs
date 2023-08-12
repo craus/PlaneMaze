f791f4d136dae6535976d0e027a93835d337cfa8
craus90 <craus902@gmail.com>
2023-08-12 21:16:00 +0400

    cursed sign: invisibility when far, root & destroy when visited

--- Assets/Scripts/Plane/Terrain/CursedSign.cs
+++ Assets/Scripts/Plane/Terrain/CursedSign.cs
@@ -4,7 +4,25 @@ using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(Figure))]
-public class CursedSign : Terrain
+public class CursedSign : Terrain, IInvisibilitySource
 {
+    public bool Invisible => (Player.instance.figure.Location.position - GetComponent<Figure>().Location.position).MaxDelta() > 2;
 
+    public override void Awake() {
+        base.Awake();
+
+        figure.collide = async (from, figure) => {
+            if (figure == null) {
+                return;
+            }
+            var victim = figure.GetComponent<Player>();
+            if (victim != null) {
+                await victim.GetComponent<Root>().Gain(2);
+                if (this == null) {
+                    return;
+                }
+                this.SoftDestroy(gameObject);
+            }
+        };
+    }
 }
