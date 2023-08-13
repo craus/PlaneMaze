35714be049e050921ce3f4c6249852001bfe0cb5
craus90 <craus902@gmail.com>
2023-08-13 07:19:31 +0400

    cursed sign indicator

--- Assets/Scripts/CursedSignIndicator.cs
+++ Assets/Scripts/CursedSignIndicator.cs
@@ -5,9 +5,29 @@ using UnityEngine;
 
 public class CursedSignIndicator : Singletone<CursedSignIndicator>
 {
-    public Sprite sprite;
+    public Animator animator;
+    public string warningBool;
+    public string dangerBool;
+    public string attackTrigger;
 
     public void Update() {
-        //Game.instance.GetComponent<CursedSignCounter>().cursedSignCount;
+        if (Game.instance == null) {
+            return;
+        }
+        var cnt = Game.instance.GetComponent<CursedSignCounter>().cursedSignCount;
+        if (cnt <= 0) {
+            animator.SetBool(warningBool, false);
+            animator.SetBool(dangerBool, false);
+        } else if (cnt < 4) {
+            animator.SetBool(warningBool, true);
+            animator.SetBool(dangerBool, false);
+        } else {
+            animator.SetBool(warningBool, true);
+            animator.SetBool(dangerBool, true);
+        }
+    }
+
+    public void Attack() {
+        animator.SetTrigger(attackTrigger);
     }
 }
