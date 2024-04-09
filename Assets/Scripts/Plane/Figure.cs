387c5a5d8c7c15baa71caaf64215a191c8fee6bf
craus90 <craus902@gmail.com>
2024-04-09 23:21:52 +0400

    remove unused usings

--- Assets/Scripts/Plane/Figure.cs
+++ Assets/Scripts/Plane/Figure.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class Figure : MonoBehaviour
 {
