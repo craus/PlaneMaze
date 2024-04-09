d019349b269c99ac12627b1c0fff9f7f13470694
craus90 <craus902@gmail.com>
2024-04-09 21:49:05 +0400

    king slime wip

---
+++ Assets/Scripts/Plane/Biomes/BitterMire.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BitterMire : MonoBehaviour, IBossGenerator
+{
+    [SerializeField] private Monster boss;
+
+    public void GenerateBoss(List<Cell> cellOrderList) {
+        Game.GenerateFigure(
+            cellOrderList.Where(cell => cell.Biome == Library.instance.bitterMire && cell.figures.Count() == 0).Rnd(),
+            boss
+        );
+    }
+}
--- Assets/Scripts/Plane/Figure.cs
+++ Assets/Scripts/Plane/Figure.cs
@@ -36,20 +36,6 @@ public class Figure : MonoBehaviour
         transform.position = location.transform.position;
     }
 
-
-    public void TryMoveWall(Cell from, Cell to) {
-        if (from.fieldCell.wall && !to.fieldCell.wall) {
-            from.fieldCell.wall = false;
-            from.UpdateCell();
-
-            to.fieldCell.wall = true;
-            to.UpdateCell();
-        }
-    }
-    public void TryMoveWall(Cell from, Vector2Int delta) {
-        TryMoveWall(from, from.Shift(delta));
-    }
-
     public async Task<bool> TryWalk(Vector2Int delta, Func<Cell, bool> free = null) {
         if (GetComponent<Root>().Current > 0) {
             return false;
@@ -62,51 +48,6 @@ public class Figure : MonoBehaviour
             return true;
         }
         return false;
-    }
-
-    private bool Deadend(Cell cell, Vector2Int delta) {
-        return
-            !cell.fieldCell.wall &&
-            //!cell.Shift(-delta).fieldCell.wall &&
-            cell.Shift(delta).fieldCell.wall &&
-            cell.Shift(delta.RotateLeft()).fieldCell.wall &&
-            cell.Shift(delta.RotateRight()).fieldCell.wall;
-    }
-
-    private bool TrySlip(Vector2Int delta) {
-        if (!Deadend(Location, delta)) {
-            return false;
-        }
-
-        var cur = Location.Shift(delta);
-        for (int i = 0; i < 200; i++) {
-            cur = cur.Shift(delta);
-            if (Deadend(cur, -delta)) {
-                Move(cur);
-                return true;
-            }
-            if (Deadend(cur, delta)) {
-                return false;
-            }
-        }
-        return false;
-    }
-
-    private void TrySwitch(Cell cell, Vector2Int direction) {
-        if (
-            cell.Shift(direction.Relative(1, 0)).Wall &&
-            cell.Shift(direction.Relative(0, 1)).Wall &&
-            cell.Shift(direction.Relative(1, 1)).Wall
-        ) {
-            cell.Shift(direction.Relative(-2, -2)).Wall ^= true;
-        }
-    }
-
-    private void TrySwitch(Cell cell) {
-        TrySwitch(cell, Vector2Int.up);
-        TrySwitch(cell, Vector2Int.right);
-        TrySwitch(cell, Vector2Int.left);
-        TrySwitch(cell, Vector2Int.down);
     }
 
     public async Task<bool> FakeMove(Vector2Int delta) {
