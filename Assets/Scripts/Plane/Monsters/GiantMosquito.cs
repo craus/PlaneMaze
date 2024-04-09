946834e38ee2ffffb4db942271a0161e18d0052c
craus90 <craus902@gmail.com>
2024-04-09 16:36:42 +0400

    giant mosquito

---
+++ Assets/Scripts/Plane/Monsters/GiantMosquito.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GiantMosquito : Monster
+{
+    public override bool Flying => true;
+
+    public override async Task AfterAttack(Vector2Int delta) {
+        await base.AfterAttack(delta);
+        await GetComponent<Health>().Heal(1);
+    }
+
+    protected override async Task MakeMove() {
+        var playerDelta = PlayerDelta;
+        if (playerDelta.SumDelta() == 1) {
+            await SmartWalk(-playerDelta);
+            return;
+        }
+        if (playerDelta.MaxDelta() == 2 && playerDelta.MinDelta() == 0) {
+            var step = playerDelta / 2;
+            if (await SmartWalk(step)) {
+                await TryAttack(step);
+            }
+            return;
+        }
+        await SmartWalk(Helpers.Moves.Rnd());
+    }
+}
